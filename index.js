791fa91c63e9b4e742dbe4fefbb48a2aca24f5ea
gturrini <gianfranco.turrini@gmail.com>
2023-01-09 15:50:38 +0000

    dded config

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+const createURL = 'http://3.8.237.60/api/create.php';
+const readAllURL = 'http://3.8.237.60/api/read_all.php';
+const readSingleURL = 'http://3.8.237.60/api/read_single.php';
+const updateURL = 'http://3.8.237.60/api/update.php';
+const deleteURL = 'http://3.8.237.60/api/delete.php';
 
 function loadTable() {
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://13.40.154.100/api/read_all.php");
+  xhttp.open("GET", readAllURL);
   xhttp.send();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      var trHTML = ''; 
+      var trHTML = '';
       const objects = JSON.parse(this.responseText).body;
       for (let object of objects) {
-        trHTML += '<tr>'; 
-        trHTML += '<td>'+object['id']+'</td>';
-        trHTML += '<td>'+object['name']+'</td>';
-        trHTML += '<td>'+object['email']+'</td>';
-        trHTML += '<td>'+object['age']+'</td>';
-        trHTML += '<td>'+object['designation']+'</td>';
-        trHTML += '<td>'+object['created']+'</td>';
-        trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox('+object['id']+')">Edit</button>';
-        trHTML += '<button type="button" class="btn btn-outline-danger" onclick="userDelete('+object['id']+')">Del</button></td>';
+        trHTML += '<tr>';
+        trHTML += '<td>' + object['id'] + '</td>';
+        trHTML += '<td>' + object['name'] + '</td>';
+        trHTML += '<td>' + object['email'] + '</td>';
+        trHTML += '<td>' + object['age'] + '</td>';
+        trHTML += '<td>' + object['designation'] + '</td>';
+        trHTML += '<td>' + object['created'] + '</td>';
+        trHTML += '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' + object['id'] + ')">Edit</button>';
+        trHTML += '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' + object['id'] + ')">Del</button></td>';
         trHTML += "</tr>";
       }
       document.getElementById("mytable").innerHTML = trHTML;
@@ -51,12 +56,12 @@ function userCreate() {
   const created = document.getElementById("created").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://13.40.154.100/api/create.php");
+  xhttp.open("POST", createURL);
   xhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
   xhttp.send(JSON.stringify({
     "name": name, "email": email, "age": age, "designation": designation, "created": created
   }));
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const objects = JSON.parse(this.responseText);
       Swal.fire(JSON.parse(objects)['message']);
@@ -68,21 +73,21 @@ function userCreate() {
 function showUserEditBox(id) {
   console.log(id);
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://13.40.154.100/api/read_single.php/?id="+id);
+  xhttp.open("GET", readSingleURL + '/?id=' + id);
   xhttp.send();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const objects = JSON.parse(this.responseText);
       const user = objects['user'];
       Swal.fire({
         title: 'Edit User',
         html:
-          '<input id="id" type="hidden" value='+objects['id']+'>' +
-          '<input id="name" class="swal2-input" placeholder="Name" value="'+objects['name']+'">' +
-          '<input id="email" class="swal2-input" placeholder="Email" value="'+objects['email']+'">' +
-          '<input id="age" class="swal2-input" placeholder="Age" value="'+objects['age']+'">' +
-          '<input id="designation" class="swal2-input" placeholder="Designation" value="'+objects['designation']+'">' +
-          '<input id="created" class="swal2-input" placeholder="Created" value="'+objects['created']+'">',
+          '<input id="id" type="hidden" value=' + objects['id'] + '>' +
+          '<input id="name" class="swal2-input" placeholder="Name" value="' + objects['name'] + '">' +
+          '<input id="email" class="swal2-input" placeholder="Email" value="' + objects['email'] + '">' +
+          '<input id="age" class="swal2-input" placeholder="Age" value="' + objects['age'] + '">' +
+          '<input id="designation" class="swal2-input" placeholder="Designation" value="' + objects['designation'] + '">' +
+          '<input id="created" class="swal2-input" placeholder="Created" value="' + objects['created'] + '">',
         focusConfirm: false,
         preConfirm: () => {
           userEdit();
@@ -101,12 +106,12 @@ function userEdit() {
   const created = document.getElementById("created").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("PUT", "http://13.40.154.100/api/update.php");
+  xhttp.open("PUT", updateURL);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify({
     "id": id, "name": name, "email": email, "age": age, "designation": designation, "created": created
   }));
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const objects = JSON.parse(this.responseText);
       Swal.fire(JSON.parse(objects)['message']);
@@ -117,12 +122,12 @@ function userEdit() {
 
 function userDelete(id) {
   const xhttp = new XMLHttpRequest();
-  xhttp.open("DELETE", "http://13.40.154.100/api/delete.php");
+  xhttp.open("DELETE", deleteURL);
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify({
     "id": id
   }));
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
       Swal.fire(JSON.parse(objects)['message']);
